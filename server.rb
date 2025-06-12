661e5235cf1358336c0d4bb13a18cf2fc02ebb78
Abdulkadir Musse <72959571+Amusse7@users.noreply.github.com>
2025-06-12 11:09:29 -0400

    Added Multithreading to server

--- server.rb
+++ server.rb
@@ -10,17 +10,24 @@ puts "Listening on port 3000..."
 loop do
   client = server.accept
 
-  request_line = client.gets
-  next if request_line.nil? || request_line.empty?
+  Thread.new do
+    begin
+      request_line = client.gets
+      next if request_line.nil? || request_line.empty?
 
-  request = HTTPRequest.new(request_line)
-  Logger.log_request(request)
+      request = HTTPRequest.new(request_line)
+      Logger.log_request(request)
 
-  response = Router.route(request)
-  client.print response.build
+      response = Router.route(request)
+      client.print response.build
 
-  client.close
-rescue => e
-  Logger.log_error(e)
-  client&.close
+      rescue => e
+        Logger.log_error(e)
+        error_response = HTTPResponse.new(500, "Internal Server Error")
+        client.print error_response.build
+    ensure
+      client.close
+    end
+  end
 end
+
